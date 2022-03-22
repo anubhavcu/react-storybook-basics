7297f5266a0d99e659c8288cdbda88c55aca2d55
anubhavcu <anubhavcs.py@gmail.com>
2022-03-22 16:14:11 +0530

    created basic stories for Button and input fields
    - also added a Subscription storie which uses other stories

---
+++ src/components/Input/Input.js
@@ -0,0 +1,7 @@
+import './input.css';
+const Input = (props) => {
+  const { size = 'medium', ...rest } = props;
+  return <input className={`input ${size}`} {...rest} />;
+};
+
+export default Input;
